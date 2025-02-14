5948aa54b560aacbb91f78ec8bfb24146617f8bf
mikolajjkrol <mikolajkock@gmail.com>
2025-02-14 13:19:17 +0100

    Menu-styles-componetnsfolder

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -1,27 +1,24 @@
 import { useRef, useState } from 'react';
 import Phaser from 'phaser';
 import { PhaserGame } from './game/PhaserGame';
-import GameConsole from './GameConsole';
+import GameConsole from './components/GameConsole';
+import StartingMenu from './components/StartingMenu';
 
 function App ()
 {
-
     //  References to the PhaserGame component (game and scene are exposed)
     const phaserRef = useRef();
     const [showConsole, setShowConsole] = useState(false);
+    const [showMenu, setShowMenu] = useState(true);
 
     return (
         <div id="app">
+            {showMenu && <StartingMenu onStart={()=>setShowMenu(false)}/>}
             <PhaserGame ref={phaserRef} />
-            <div>
-                <div>
-                    <button className="button" onClick={() => setShowConsole(prev => !prev)}>
-                        Code Editor
-                    </button>
-                </div>
-
-                {showConsole && <GameConsole gameRef={phaserRef} onClose={() => setShowConsole(false)} />}
-            </div>
+            <button className="button" onClick={() => setShowConsole(prev => !prev)}>
+                Code Editor
+            </button>
+            {showConsole && <GameConsole gameRef={phaserRef} onClose={() => setShowConsole(false)} />}
         </div>
     );
 }
