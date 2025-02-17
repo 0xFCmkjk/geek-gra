34aa22e00c4f0a36d3aab157fe9f3a314cff6952
mkjk <miszka1172@gmail.com>
2025-02-17 13:04:45 +0100

    Added phaser and react docs buttons

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -10,14 +10,24 @@ function App ()
     const phaserRef = useRef();
     const [showConsole, setShowConsole] = useState(false);
     const [showMenu, setShowMenu] = useState(true);
+    const phaserDocsUrl = "https://docs.phaser.io/phaser/getting-started/what-is-phaser";
+    const reactDocsUrl = "https://react.dev/reference/react";
 
     return (
         <div id="app">
             {showMenu && <StartingMenu onStart={()=>setShowMenu(false)}/>}
             <PhaserGame ref={phaserRef} isConsoleOpen={showConsole} />
-            <button className="button" onClick={() => setShowConsole(prev => !prev)}>
-                Code Editor
-            </button>
+            <div>
+                <button className="button" onClick={() => setShowConsole(prev => !prev)}>
+                    Code Editor
+                </button>
+                <button className="button" onClick={() => window.open(phaserDocsUrl, '_blank').focus()}>
+                    Phaser Docs
+                </button>
+                <button className="button" onClick={() => window.open(reactDocsUrl, '_blank').focus()}>
+                    React Docs
+                </button>
+            </div>
             {showConsole && <GameConsole gameRef={phaserRef} onClose={() => setShowConsole(false)} />}
         </div>
     );
