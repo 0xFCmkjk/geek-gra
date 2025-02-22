42dcce6581ce4d39d2a80cd6e7708d8ddf4475e9
mikolajjkrol <mikolajkock@gmail.com>
2025-02-22 16:15:45 +0100

    menu

--- src/src/components/StartingMenu.jsx
+++ src/src/components/StartingMenu.jsx
@@ -4,8 +4,8 @@ export default function StartingMenu({onStart}) {
         <div className="starting-menu">
             <div className="menu">
                 <h1>Welcome to the Nodebreaker game!</h1>
-                <button onClick={onStart} className='menuBtn'>Start</button>
-                <button onClick={onStart} className='menuBtn'>Info</button>
+                <button onClick={onStart} className='menuBtn'>Start Game</button>
+                <button onClick={onStart} className='menuBtn'>Information</button>
                 <button onClick={onStart} className='menuBtn'>Credientials</button>
                 <button onClick={(localStorage.clear())} className='menuBtn'>Reset Progress</button>
             </div>
