da34925f400e56ebbcdaa30054c97dae1945677c
mkjk <miszka1172@gmail.com>
2025-02-22 18:00:17 +0100

    another update

--- src/src/components/Credits.jsx
+++ src/src/components/Credits.jsx
@@ -3,7 +3,7 @@ export default function Credits({onClose}){
         <div className="menu">
                 <h2>Credientials</h2>
                 <p>Michał Bartoszcze</p>
-                <i>JavaScript, Game backend, Phaser integration</i>
+                <i>JavaScript, Game backend, Gameplay</i>
                 <p>Mikołaj Król</p>
                 <i>React components, graphics, game frontend</i>
                 <br /><br />
