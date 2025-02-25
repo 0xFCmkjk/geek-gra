225b32d382e3bb22414446256cc2aa0d770a0a94
mikolajjkrol <mikolajkock@gmail.com>
2025-02-25 20:16:11 +0100

    start-quiz

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App ()
         setShowConsole(false);
     })
 
-    EventBus.on('quiz-start',(e)=>{
+    EventBus.on('start-quiz',(e)=>{
         setShowQuiz(true);
         quizNum = e;
     })
