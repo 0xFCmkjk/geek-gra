1960ebf3e3465330397ed9dc4ace1b516400a279
mikolajjkrol <mikolajkock@gmail.com>
2025-02-27 17:13:32 +0100

    q

--- src/game/main.js
+++ src/game/main.js
@@ -11,8 +11,8 @@ import { AUTO, Game } from 'phaser';
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config = {
     type: AUTO,
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: 1700,
+    height: 850,
     physics: {
         default: 'arcade',
         arcade: {
